e0efca8ce919be731db945066f06eefd22ca9032
Logon Aluno <logonaluno@local.com>
2019-12-11 22:08:02 -0300

    first commit

---
+++ src/main/java/fiap/scj34/rest/controllers/VehicleController.java
@@ -0,0 +1,18 @@
+package fiap.scj34.rest.controllers;
+
+import java.util.List;
+
+import fiap.scj34.rest.models.Vehicle;
+
+public class VehicleController {
+	
+	public List<Vehicle> getAllVehicle(){}
+	
+	public Vehicle getVehicleById(Integer id) {}
+	
+	public void saveVehicle(Vehicle vehicle) {}
+	
+	public Vehicle updateVehicle(Integer id, Vehicle vehicle) {}
+	
+	public Vehicle deleteVehicle(Integer id) {}
+}
---
+++ src/main/java/fiap/scj34/rest/models/Vehicle.java
@@ -0,0 +1,51 @@
+package fiap.scj34.rest.models;
+
+public class Vehicle {
+	
+	private Integer id;
+	private String brand;
+	private String model;
+	private String plate;
+	private Integer year;
+	private String color;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getPlate() {
+		return plate;
+	}
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+
+}
